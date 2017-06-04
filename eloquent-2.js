603a381d2bbe5f6875035034253f42ef8949116e
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-06-05 10:45:07 +1200

    Update eloquent-2.js
    Exercises for http://eloquentjavascript.net

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,1 +1,47 @@
 eloquentjavascript
+
+/* Looping a triangle
+
+var hash ="";
+for (var i =1;i<8;i++) {
+hash += "#";
+console.log(hash);
+}
+
+*/
+
+
+/* FizzBuzz
+
+for (i=1; i<=100;i++){
+if (i % 3 === 0) {
+console.log("Fizz");
+} else if (i % 5 === 0){
+console.log("Buzz");
+} else if (i % 3 ===0 && i % 5 ===0){
+console.log("FizzBuzz");
+} else {
+console.log(i);
+}
+}
+
+*/
+
+/* Chess board
+
+var size = 8; 
+var space = "";
+
+for (var i = 0; i < size; i++) {  
+  for (var j = 0; j< size; j++) {
+    if ((i + j) % 2 == 0)
+      space += " ";
+    else
+      space += "#";
+  }
+  space += "\n";
+}
+
+console.log(space);
+
+*/
